085a4dea2716d88473e03ecdf453c2a68345dd11
Karol Szeląg <118350099+karo23361@users.noreply.github.com>
2025-05-05 10:47:23 +0200

    Update and rename Kafka_Stream.py to PostgreSQL_Stream.py

--- PostgreSQL_Stream.py
+++ PostgreSQL_Stream.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split
 from pyspark.sql.types import IntegerType, StringType
 
-# Tworzymy SparkSession z wymaganymi bibliotekami
+
 spark = SparkSession.builder \
     .appName("KafkaToPostgres") \
     .master("local[*]") \
@@ -11,11 +11,11 @@ spark = SparkSession.builder \
             "org.postgresql:postgresql:42.7.4") \
     .getOrCreate()
 
-# Ustawienia Kafka
+
 kafka_bootstrap_servers = "kafka:29092"
 kafka_topic = "csv_records"
 
-# Wczytanie strumienia z Kafki
+
 df_raw = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
@@ -33,7 +33,7 @@ processed_df = df_raw.selectExpr("CAST(value AS STRING)") \
         "split(value, ',')[6] AS content"
     )
 
-# Funkcja do przetwarzania i zapisywania danych do PostgreSQL
+
 def process_batches(df, epoch_id):
     df.write \
         .format("jdbc") \
@@ -46,12 +46,11 @@ def process_batches(df, epoch_id):
         .save()
     print(f"✅ Batch {epoch_id} zapisany do PostgreSQL.")
 
-# Uruchomienie strumienia i zapis danych do PostgreSQL
+
 query = processed_df.writeStream \
     .foreachBatch(process_batches) \
     .outputMode("append") \
     .option("checkpointLocation", "checkpoint") \
     .start()
 
-# Czekaj na zakończenie strumienia
 query.awaitTermination()
