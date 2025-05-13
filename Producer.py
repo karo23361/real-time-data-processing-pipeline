4aab38dfbf75c9a65052c4e80dd5ba5821faa507
Karol Szeląg <118350099+karo23361@users.noreply.github.com>
2025-05-13 10:05:32 +0200

    Update Producer.py

--- Producer.py
+++ Producer.py
@@ -2,23 +2,18 @@ from kafka import KafkaProducer
 import csv
 import time
 
-# Tworzymy producenta Kafka
-producer = KafkaProducer(bootstrap_servers='kafka:29092')  # używamy 'kafka:29092' w Dockerze
+producer = KafkaProducer(bootstrap_servers='kafka:29092')  
 
 # Otwórz plik CSV
 with open('HDFS_2k.log_structured.csv', mode='r') as file:
-    csv_reader = csv.DictReader(file)  # Czytamy wiersze jako słowniki
+    csv_reader = csv.DictReader(file)  
     for row in csv_reader:
-        # Tworzymy wiadomość
         message = f"{row['LineId']},{row['Date']},{row['Time']},{row['Pid']},{row['Level']},{row['Component']},{row['Content']}".encode('utf-8')
         
-        # Wysyłamy do topicu 'csv_records'
         producer.send('csv_records', message)
         print(f"Sent: {message.decode('utf-8')}")
         
-        # Odczekaj 1 sekundę
         time.sleep(1)
 
-# Czekamy, aż wszystkie wiadomości zostaną wysłane
 producer.flush()
 
