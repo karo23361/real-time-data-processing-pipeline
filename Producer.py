4f8d408e7b0b3e4fe525f9e004b94c1061e96c5e
Karol Szeląg <118350099+karo23361@users.noreply.github.com>
2025-05-27 11:15:29 +0200

    Update Producer.py

--- Producer.py
+++ Producer.py
@@ -5,15 +5,13 @@ import time
 producer = KafkaProducer(bootstrap_servers='kafka:29092')  
 
 # Otwórz plik CSV
-with open('HDFS_2k.log_structured.csv', mode='r') as file:
+with open('logs_1.csv', mode='r') as file:
     csv_reader = csv.DictReader(file)  
     for row in csv_reader:
         message = f"{row['LineId']},{row['Date']},{row['Time']},{row['Pid']},{row['Level']},{row['Component']},{row['Content']}".encode('utf-8')
         
         producer.send('csv_records', message)
-        print(f"Sent: {message.decode('utf-8')}")
-        
-        time.sleep(1)
+        #print(f"Sent: {message.decode('utf-8')}")
 
 producer.flush()
 
